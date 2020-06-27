380d558153377f9e865af31bde08ddfe8fef6a5c
Drew Mercer <japanexpress@gmail.com>
2020-06-27 09:15:37 -0400

    Adding logo and repo link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './ymtp-logo-hologram.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>COMING SOON</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://github.com/drewwmercer/you-make-the-play'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          You Make the Play
+          GitHub
         </a>
       </header>
     </div>
