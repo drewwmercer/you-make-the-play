944beb4996864e17ea16cdd0b7d8b9b1b149972e
Drew Mercer <japanexpress@gmail.com>
2020-06-27 19:05:00 -0400

    Changing component styles and adding use of Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './ymtp-logo-hologram.png';
+import logo from './assets/ymtp-logo-hologram.png';
 import './App.css';
 import { Game } from './Game';
 import { Rules } from './Rules';
@@ -8,43 +8,45 @@ import { Cards } from './Cards';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavigationBar';
+import { NavigationBar } from './NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavigationBar />
-      <Jumbotron />
-      <Layout>
-        <div className='App'>
-          {/* <header className='App-header'> */}
-          <img src={logo} className='App-logo' alt='logo' />
-
-          <p>COMING SOON</p>
-          <a
-            className='App-link'
-            href='https://github.com/drewwmercer/you-make-the-play'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            GitHub
-          </a>
-          {/* </header> */}
-        </div>
-
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
         <Router>
-          <Switch>
-            <Route exact path='/' component={Game} />
-            <Route path='/rules' component={Rules} />
-            <Route path='/cards' component={Cards} />
-            <Route path='/contact' component={Contact} />
-            <Route component={NoMatch} />
-          </Switch>
+          <NavigationBar />
+          <Jumbotron />
+          <Layout>
+            <div className='App'>
+              {/* <header className='App-header'> */}
+              <img src={logo} className='App-logo' alt='logo' />
+
+              <p>COMING SOON</p>
+              <a
+                className='App-link'
+                href='https://github.com/drewwmercer/you-make-the-play'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                GitHub
+              </a>
+              {/* </header> */}
+            </div>
+
+            <Switch>
+              <Route exact path='/' component={Game} />
+              <Route path='/rules' component={Rules} />
+              <Route path='/cards' component={Cards} />
+              <Route path='/contact' component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
         </Router>
-      </Layout>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,7 +3,27 @@ import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import fieldImage from '../assets/field.jpg';
 
-const Styles = styled.div``;
+const Styles = styled.div`
+    .jumbo {
+        background: url(${fieldImage}) no-repeat fixed bottom;
+        background-size: cover;
+        color: #efefef;
+        height: 200px;
+        position: relative;
+        z-index: -2;
+    }
+
+    .overlay {
+        background-color: #000;
+        opacity: 0.2;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+    }
+`;
 
 export const Jumbotron = () => (
     <Styles>
