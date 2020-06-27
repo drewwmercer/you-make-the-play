2edeaaa3491a4d1bf605b2526b87040286007582
Drew Mercer <japanexpress@gmail.com>
2020-06-27 19:20:40 -0400

    Moving navbar items to left and adding mobile divider

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Cards } from './Cards';
 import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
-import { NavigationBar } from './NavigationBar';
+import { NavigationBar } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
 
 class App extends Component {
