ffcdffcf404ce53f1629dec23220f4f20f06e3e5
Drew Mercer <japanexpress@gmail.com>
2020-06-27 16:58:32 -0400

    updates to npm and added ract bootstrap

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+
         <p>COMING SOON</p>
         <a
           className='App-link'
