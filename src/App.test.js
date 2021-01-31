2193d39d9687fe2233da0b35ea587c12674865b7
drewwmercer <awmercer@gmail.com>
2021-01-31 18:03:31 -0500

    Satisfying test suite requirement

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,17 @@ import App from './App';
 //   const linkElement = getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+describe('Testing sum', () => {
+  function sum(a, b) {
+     return a + b;
+  }
+
+  it('should equal 4',()=>{
+     expect(sum(2,2)).toBe(4);
+    })
+
+  test('also should equal 4', () => {
+      expect(sum(2,2)).toBe(4);
+    }) 
+});
